0c5d60975b3bbcd393be3cd5e6f969fc580f7bb1
theTrickster123 <tarissi.mehdi2019@gmail.com>
2025-04-01 18:02:25 +0000

    Initial commit

---
+++ src/main/java/dev/trickster123/cashcard/CashCard.java
@@ -0,0 +1,4 @@
+package dev.trickster123.cashcard;
+
+public record CashCard(Long id, Double amount) {
+}
---
+++ src/main/java/dev/trickster123/cashcard/CashCardAppApplication.java
@@ -0,0 +1,13 @@
+package dev.trickster123.cashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashCardAppApplication.class, args);
+	}
+
+}
