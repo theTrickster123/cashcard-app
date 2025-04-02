3309593585eb251d3860e14e57af49f523d12891
theTrickster123 <tarissi.mehdi2019@gmail.com>
2025-04-02 01:20:06 +0000

    Corrected tests

---
+++ src/test/java/dev/trickster123/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.trickster123.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
